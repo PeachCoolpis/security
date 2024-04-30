2c281aa5d8e3c69f5676fda948e7b978ede699a0
PeachCoolPiece <121959157+PeachCoolPiece@users.noreply.github.com>
2024-04-30 22:10:02 +0900

    AuthenticationProvider 설정

---
+++ src/main/java/io/security/springsecuritymaster/security/CustomAuthenticationProvider.java
@@ -0,0 +1,34 @@
+package io.security.springsecuritymaster.security;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.List;
+
+
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+    
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        
+        String loginId = authentication.getName(); // 로그인 아이디
+        String password = (String) authentication.getCredentials(); // 비밀번호
+        System.out.println("클래스패스 = " + System.getProperty("java.class.path"));
+        
+        // 아이디 검증
+        
+        // 비밀번호 검증
+        
+        return new UsernamePasswordAuthenticationToken(loginId, password, List.of(new SimpleGrantedAuthority("ROLE_USER")));
+        // 토큰 타입으로 리턴
+    }
+    
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.isAssignableFrom(UsernamePasswordAuthenticationToken.class);
+    }
+}
--- src/main/java/io/security/springsecuritymaster/security/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/security/SecurityConfig.java
@@ -1,25 +1,22 @@
 package io.security.springsecuritymaster.security;
 
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
-import org.springframework.security.web.savedrequest.SavedRequest;
-
-import java.io.IOException;
 
 @EnableWebSecurity
 @Configuration
@@ -27,19 +24,41 @@ public class SecurityConfig {
     
     
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http
+            , AuthenticationManagerBuilder builder
+            , AuthenticationConfiguration configuration) throws Exception {
         
-       
+        AuthenticationManagerBuilder managerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        managerBuilder.authenticationProvider(coustomAuthenticationProvider());
+        
+        ProviderManager authenticationManager = (ProviderManager) configuration.getAuthenticationManager();
+        authenticationManager.getProviders().remove(0); // parant에 있는것 삭제
+        builder.authenticationProvider(new DaoAuthenticationProvider());
+        
+        //AuthenticationManagerBuilder managerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        //managerBuilder.authenticationProvider(new CustomAuthenticationProvider());
+        //managerBuilder.authenticationProvider(new CustomAuthenticationProvider2()); 아래에 있는 http.authenticationProvider 에 추가해도 동일
+        
+        
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/logoutSuccess").permitAll()
                         .anyRequest()
                         .authenticated())
                 .formLogin(Customizer.withDefaults())
+//                .authenticationProvider(new CustomAuthenticationProvider())
+//                .authenticationProvider(new CustomAuthenticationProvider2())
+                
+                
         
                 
         ;
         return http.build();
+    }
+    
+    @Bean
+    public AuthenticationProvider coustomAuthenticationProvider() {
+        return new CustomAuthenticationProvider();
     }
     
     @Bean
@@ -57,5 +76,6 @@ public class SecurityConfig {
 //                .roles("USER")
 //                .build();
         return new InMemoryUserDetailsManager(user1);
+        
     }
 }
