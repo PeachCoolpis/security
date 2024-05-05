8ea3b1b1b8239210ef3f46f6654d507f40aafdde
PeachCoolPiece <121959157+PeachCoolPiece@users.noreply.github.com>
2024-05-06 00:49:18 +0900

    계층적 권한 - RoleHirerachy

--- src/main/java/io/security/springsecuritymaster/controller/IndexController.java
+++ src/main/java/io/security/springsecuritymaster/controller/IndexController.java
@@ -1,25 +1,35 @@
 package io.security.springsecuritymaster.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.CurrentSecurityContext;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class IndexController {
     
+    
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "index";
     }
- 
+    
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+    
+    @GetMapping("/db")
+    public String db() {
+        return "db";
+    }
+    
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+    
 }
 
--- src/main/java/io/security/springsecuritymaster/security/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/security/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,27 +21,38 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
-@EnableMethodSecurity(securedEnabled = true , jsr250Enabled = true)
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 @Configuration
 public class SecurityConfig {
     
     private final String[] paths = {"/css/**", "/images/**", "/js/**", "webjars/**", "/favicon.*", "/*/icon-*"};
     
-//    @Bean
-//    public WebSecurityCustomizer webSecurityCustomizer() {
-//        return web -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-//    }
-
+    
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(paths).permitAll()
+                        .requestMatchers("/user").hasRole("USER")
+                        .requestMatchers("/db").hasRole("DB")
+                        .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().permitAll()) // 요청 기반 권한요청
                 .formLogin(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
         ;
         return http.build();
+    }
+    
+    
+    @Bean
+    public RoleHierarchy roleHierarchy() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        String role1 = "ROLE_ADMIN > ROLE_DB\n";
+        String role2 = "ROLE_DB > ROLE_USER\n";
+        String role3 = "ROLE_USER > ROLE_ANONYMOUS";
+        
+        String role = role1 + role2 + role3;
+        roleHierarchy.setHierarchy(role);
+        return roleHierarchy;
     }
     
     @Bean
