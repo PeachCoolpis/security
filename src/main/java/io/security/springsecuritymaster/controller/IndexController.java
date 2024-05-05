25d883a02c36be7a45f54ed405842fd3b217b24d
PeachCoolPiece <121959157+PeachCoolPiece@users.noreply.github.com>
2024-05-06 00:25:27 +0900

    정적 자원 관리

--- src/main/java/io/security/springsecuritymaster/controller/IndexController.java
+++ src/main/java/io/security/springsecuritymaster/controller/IndexController.java
@@ -8,10 +8,11 @@ import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class IndexController {
     
--- src/main/java/io/security/springsecuritymaster/security/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/security/SecurityConfig.java
@@ -1,13 +1,16 @@
 package io.security.springsecuritymaster.security;
 
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,12 +23,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
     
+    private final String[] paths = {"/css/**", "/images/**", "/js/**", "webjars/**", "/favicon.*", "/*/icon-*"};
     
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return web -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+//    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(paths).permitAll()
                         .anyRequest().permitAll()) // 요청 기반 권한요청
                 .formLogin(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
